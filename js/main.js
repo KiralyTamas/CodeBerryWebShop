d7afa20ca6311be4e2f74f0052b0fe4706359e9a
Király Tamás <tomaszpest@gmail.com>
2021-09-21 20:33:45 +0200

    Haladunk

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
+'use strict';
 class UI {
-    constructor(domImg, domCim, domAltCim, domDescription, domPrize) {
-        domImg = document.getElementsByName("img").src;
-        domCim = document.getElementsByClassName("name").value;
-        domAltCim = document.getElementsByClassName("altName");
-        domDescription = document.getElementsByClassName("description");
-        domPrize = document.getElementsByClassName("prize");
-        this.kep = domImg;
-        this.cim = domCim;
-        this.altCim = domAltCim;
-        this.description = domDescription;
-        this.prize = domPrize;
-    }
+  constructor(domImg, domCim, domAltCim, domDescription, domPrize) {
+    domImg = document.getElementsByName("img").src;
+    domCim = document.querySelector(".name");
+    domAltCim = document.getElementsByClassName("altName");
+    domDescription = document.getElementsByClassName("description");
+    domPrize = document.getElementsByClassName("prize");
+    this.kep = domImg;
+    this.cim = domCim;
+    this.altCim = domAltCim;
+    this.description = domDescription;
+    this.prize = domPrize;
+  }
 }
 class productInfo extends UI {
-    constructor(cim) {
-        super(cim);
-        this.productPicture = "/CodeBerryWebShop/assets/discatcher.jpg";
-        this.productName = "DisCatcher Target";
-        this.productAltName = "Discgolf";
-        this.productDescription = "DisCatcher Target a chain grid that catches fast and slow putts, heavy and light discs like no other target.";
-        this.productPrize = "399";
-        this.euro = "€"
-    }
-}
-productInfo.prototype.feel = function () {
+  constructor(cim, productName) {
+    super(cim);
+    this.productPicture = "/CodeBerryWebShop/assets/discatcher.jpg";
+    productName = "DisCatcher Target";
+    this.productAltName = "Discgolf";
+    this.productDescription = "DisCatcher Target a chain grid that catches fast and slow putts, heavy and light discs like no other target.";
+    this.productPrize = "399";
+    this.euro = "€"
+  }
+  feel() {
     this.cim.innerHTML = this.productName;
-};
-new productInfo().feel();
+  }
+}
+const start = new productInfo();
+start.feel();
