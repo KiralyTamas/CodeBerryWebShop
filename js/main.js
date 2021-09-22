72ba7f1f4d1052154cddcad60ad426753bfc4d85
KiralyTamas <tomaszpest@gmail.com>
2021-09-22 11:32:10 +0200

    Még a képet kell beilleszteni valahogy.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 'use strict';
 class UI {
-  constructor(domImg, domCim, domAltCim, domDescription, domPrize) {
-    domImg = document.querySelector(".img");
-    domCim = document.querySelector(".name");
-    domAltCim = document.querySelector("altName");
-    domDescription = document.querySelector("description");
-    domPrize = document.querySelector("prize");
-    this.kep = domImg;
-    this.cim = domCim;
-    this.altCim = domAltCim;
-    this.description = domDescription;
-    this.prize = domPrize;
-  }
+    constructor(domImg, domCim, domAltCim, domDescription, domPrize) {
+        domImg = document.getElementById("picture").src;
+        domCim = document.querySelector(".name");
+        domAltCim = document.querySelector(".altName");
+        domDescription = document.querySelector(".description");
+        domPrize = document.querySelector(".prize");
+        this.kep = domImg;
+        this.cim = domCim;
+        this.altCim = domAltCim;
+        this.description = domDescription;
+        this.prize = domPrize;
+    }
 }
 class productInfo extends UI {
-  constructor(cim) {
-    super(cim);
-    this.productPicture = "/CodeBerryWebShop/assets/discatcher.jpg";
-    this.productName = "DisCatcher Target";
-    this.productAltName = "Discgolf";
-    this.productDescription = "DisCatcher Target a chain grid that catches fast and slow putts, heavy and light discs like no other target.";
-    this.productPrize = "399";
-    this.euro = "€"
-  }
-  feel() {
-    this.cim.innerHTML = this.productName;
-
-  }
+    constructor(kep, cim, altCim, description, prize) {
+        super(kep, cim, altCim, description, prize);
+        this.productPicture = "/CodeBerryWebShop/assets/discatcher.jpg";
+        this.productName = "DisCatcher Target";
+        this.productAltName = "Discgolf";
+        this.productDescription = "DisCatcher Target a chain grid that catches fast and slow putts, heavy and light discs like no other target.";
+        this.productPrize = "399";
+        this.euro = "€"
+    }
+    feel() {
+        this.kep = this.productPicture;
+        this.cim.innerHTML = this.productName;
+        this.altCim.innerHTML = this.productAltName;
+        this.description.innerHTML = this.productDescription;
+        this.prize.innerHTML = this.euro + this.productPrize;
+    }
 }
 const start = new productInfo();
 start.feel();
